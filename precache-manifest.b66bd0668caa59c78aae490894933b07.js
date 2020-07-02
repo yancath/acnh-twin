62c9fac3c9f64a1e30940c1fb84278e99c40bc17
yancath <catherine.yan01@gmail.com>
2020-07-02 18:51:33 -0400

    Updates

---
+++ precache-manifest.b66bd0668caa59c78aae490894933b07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5497efd1ed4a255da28ea7dc06a7b02f",
+    "url": "/acnh-twin/index.html"
+  },
+  {
+    "revision": "1169b693b482e0432ec6",
+    "url": "/acnh-twin/static/css/main.dbb6c5b8.chunk.css"
+  },
+  {
+    "revision": "df28efe4ccffd3a01470",
+    "url": "/acnh-twin/static/js/2.e0e8efc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/acnh-twin/static/js/2.e0e8efc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1169b693b482e0432ec6",
+    "url": "/acnh-twin/static/js/main.26abaf9d.chunk.js"
+  },
+  {
+    "revision": "5f4a2aad338388d63869",
+    "url": "/acnh-twin/static/js/runtime-main.b2188955.js"
+  }
+]);
